0361dec36d9178a82c1c032e9389810e32fa1c47
saadullah01 <saadullah1912@gmail.com>
2021-03-24 05:02:25 +0000

    Add the @testing-library/cypress dependency.

--- services/web/cypress/integration/spec.js
+++ services/web/cypress/integration/spec.js
@@ -9,6 +9,11 @@ describe("Sapper template app", () => {
       cy.contains("h1", "Great success!");
     });
   
+    it("has the correct heading", () => {
+      // https://testing-library.com/docs/guide-which-query
+      cy.findByRole("heading").should("contain", "Great success!");
+    });
+
     it("navigates to /about", () => {
       cy.get("nav a").contains("about").click();
       cy.url().should("include", "/about");
@@ -36,4 +41,4 @@ describe('Blog posts', () => {
           .should('have.attr', 'href', `blog/${post.slug}`)
       })
     });
-  });
+});
